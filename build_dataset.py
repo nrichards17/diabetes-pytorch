76ea57de43bfe8e069a482cbdb7481e1d44abd94
Nathaniel <nathaniel.richards17@gmail.com>
2018-10-20 17:45:15 -0400

    Complete df embedding vai sklearn LabelEncoder

--- build_dataset.py
+++ build_dataset.py
@@ -2,6 +2,10 @@ import argparse
 import os
 
 import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+
+from sklearn.preprocessing import LabelEncoder
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--raw_data_path', default='data/raw/diabetic_data.csv', help="Directory with the raw diabetes dataset")
@@ -10,12 +14,16 @@ parser.add_argument('--output_dir', default='data/processed', help="Where to wri
 SEED = 42
 PCT_THRESHOLD = 95
 
-ignore_features = [
+IGNORE_FEATURES = [
     'encounter_id',
     'patient_nbr',
 ]
 
-continuous_features = [
+KEEP_FEATURES = [
+    'weight',
+]
+
+CONTINOUS_FEATURES = [
     'time_in_hospital',
     'num_lab_procedures',
     'num_procedures',
@@ -26,7 +34,7 @@ continuous_features = [
     'number_diagnoses',
 ]
 
-output_features = ['readmitted']
+OUTPUT_FEATURES = ['readmitted']
 
 
 def identify_imbalanced_categories(df, categorical_features, pct_threshold):
@@ -38,40 +46,61 @@ def identify_imbalanced_categories(df, categorical_features, pct_threshold):
 
     return imbalanced
 
-
-if __name__ == '__main__':
-    args = parser.parse_args()
-    assert os.path.isfile(args.raw_data_path), "Couldn't find the dataset at {}".format(args.raw_data_path)
-
-    print('Reading csv from: {}'.format(args.raw_data_path))
-    raw_df = pd.read_csv(args.raw_data_path, na_values='?')
-    print('Replacing "NO" with ">30"')
-    raw_df['readmitted'] = raw_df['readmitted'].replace({'NO': '>30'})
-
+def downsample_data(df, feature, minority_class, majority_class):
     # number of cases readmitted <30 - minority class
-    num_minor_samples = raw_df['readmitted'].value_counts()['<30']
+    num_minor_samples = df[feature].value_counts()[minority_class]
 
-    minority_df = raw_df[raw_df['readmitted'] == '<30']
-    majority_df = raw_df[raw_df['readmitted'] == '>30']
+    minority_df = df[df[feature] == minority_class]
+    majority_df = df[df[feature] == majority_class]
 
     # downsample majority class to same as minority class
-    majority_downsampled_df = majority_df.sample(num_minor_samples,
-                                                 replace=False,
-                                                 random_state=SEED)
+    majority_downsampled_df = majority_df.sample(num_minor_samples, replace=False, random_state=SEED)
 
     # create new df w/ 50/50 classes
     downsampled_df = pd.concat([minority_df, majority_downsampled_df])
     # shuffle new df and reset index
     downsampled_df = downsampled_df.sample(frac=1, random_state=SEED).reset_index(drop=True)
 
+    return downsampled_df
 
 
+if __name__ == '__main__':
+    args = parser.parse_args()
+    assert os.path.isfile(args.raw_data_path), "Couldn't find the dataset at {}".format(args.raw_data_path)
 
+    print('Reading csv from: {}'.format(args.raw_data_path))
+    raw_df = pd.read_csv(args.raw_data_path, na_values='?', low_memory=False)
 
-    categorical_features = [var for var in raw_df.columns
-                            if var not in ignore_features
-                            and var not in continuous_features
-                            and var not in output_features]
+    # replace NO with >30
+    raw_df['readmitted'] = raw_df['readmitted'].replace({'NO': '>30'})
 
-    imbalanced_features = identify_imbalanced_categories(raw_df, categorical_features, PCT_THRESHOLD)
+    downsampled_df = downsample_data(raw_df,
+                                     feature='readmitted',
+                                     minority_class='<30',
+                                     majority_class='>30')
+
+    categorical_features_all = [var for var in raw_df.columns
+                                if var not in IGNORE_FEATURES
+                                and var not in CONTINOUS_FEATURES
+                                and var not in OUTPUT_FEATURES]
+
+    imbalanced_features = identify_imbalanced_categories(downsampled_df, categorical_features_all, PCT_THRESHOLD)
+
+    CATEGORICAL_FEATURES = [var for var in categorical_features_all
+                            if var not in imbalanced_features
+                            or var in KEEP_FEATURES]
+
+    # print(categorical_features_all)
+    # print(imbalanced_features)
+    # print(CATEGORICAL_FEATURES)
+
+    embedded_df = downsampled_df[OUTPUT_FEATURES + CONTINOUS_FEATURES + CATEGORICAL_FEATURES]
+    embedded_df['readmitted'] = embedded_df['readmitted'].replace({'>30': 0, '<30': 1})
+
+    for cat_col in CATEGORICAL_FEATURES:
+        print(cat_col)
+        embedded_df[cat_col] = LabelEncoder().fit_transform(embedded_df[cat_col].astype(str))
+
+    print(embedded_df)
+    print(embedded_df.dtypes)
 
